5779b3d276da1b2d396b559e3ab8e6f70f1d132b
Amnaa-N <139056272+Amnaa-N@users.noreply.github.com>
2025-03-03 13:12:03 +0500

    Update energyapp.py

--- energyapp.py
+++ energyapp.py
@@ -22,7 +22,7 @@ st.markdown(
         background-color: grey; 
     }
     .stApp {
-            background-color:#a88780 ; 
+            background-color:#a76f63 ; 
         color:white;
         padding: 20px;
         border-radius: 10px;
